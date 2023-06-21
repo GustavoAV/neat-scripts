6ad93a3556ba7ef76dcd6f9124b431eb58723b4e
Gustavo Vasconcelos <gustavo.vasconcelos@f13.com.br>
2023-06-21 09:15:04 -0300

    feat: Improve overall and add exception list

--- scripts/rainbow_cowsay.sh
+++ scripts/rainbow_cowsay.sh
@@ -10,10 +10,26 @@
 #   ./rainbow_cowsay fortune
 #    To make it run whenever you open your terminal, put a line with a call to this script at the end of your "~/.bashrc".
 
-cows=( $(cowsay -l | sed '1d') )
+set -euo pipefail
 
-# Select random item of array in bash: https://stackoverflow.com/a/2388555
-random_cow=${cows[$RANDOM % ${#cows[@]}]}
+cows_path=/usr/share/cowsay/cows/
+
+find_args=(-type f -name "*.cow")
+
+# You can add cow exceptions separated by space if needed
+# "kiss" is already there because it's kinda awkward
+exception_list=(kiss duck)
+
+# Add cow exceptions to find
+for exception in "${exception_list[@]}"; do
+    find_args+=(! -name "$exception".cow)
+done
+
+# Select random cow
+random_cow_file=$(find ${cows_path} "${find_args[@]}" | shuf -n 1)
+
+# Remove extension
+random_cow=$(basename "${random_cow_file}" .cow)
 
 # Make your input *~fabulous~*
 "$@" | cowsay -f "$random_cow" | lolcat
