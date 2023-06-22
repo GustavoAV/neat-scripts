c5c7d70d8683946d10a793545eaa342e9618679a
Gustavo Vasconcelos <gustavo.vasconcelos@f13.com.br>
2023-06-22 08:29:55 -0300

    feat: Add fortune when provided no args
    Also add dependencies check

--- scripts/rainbow_cowsay.sh
+++ scripts/rainbow_cowsay.sh
@@ -6,14 +6,24 @@
 #   Here's the Github to it: https://github.com/busyloop/lolcat
 # USAGE:
 #   Use the examples below and see the magic happen:
-#   ./rainbow_cowsay echo -n "Hello World!"
-#   ./rainbow_cowsay fortune
+#   ./rainbow_cowsay
+#   ./rainbow_cowsay Hello World!
 #    To make it run whenever you open your terminal, put a line with a call to this script at the end of your "~/.bashrc".
 
 set -euo pipefail
 
-cows_path=/usr/share/cowsay/cows/
+dependencies_check() {
+    for dependency in "$@"; do
+        if ! command -v "${dependency}" > /dev/null 2>&1; then
+            echo "${dependency} is not installed!" >&2
+            exit 1
+        fi
+    done
+}
 
+dependencies_check cowsay lolcat
+
+cows_dir=/usr/share/cowsay/cows/
 find_args=(-type f -name "*.cow")
 
 # You can add cow exceptions separated by space if needed
@@ -26,12 +36,16 @@ for exception in "${exception_list[@]}"; do
 done
 
 # Select random cow
-random_cow_file=$(find ${cows_path} "${find_args[@]}" | shuf -n 1)
+random_cow_file=$(find ${cows_dir} "${find_args[@]}" | shuf -n 1)
 
 # Remove extension
 random_cow=$(basename "${random_cow_file}" .cow)
 
-# Make your input *~fabulous~*
-"$@" | cowsay -f "$random_cow" | lolcat
+# When provided arguments, echoes the input
+if [[ $# -gt 0 ]]; then
+    echo "$@" | cowsay -f "$random_cow" | lolcat
+else
+    fortune -s | cowsay -f "$random_cow" | lolcat
+fi
 
 exit 0
